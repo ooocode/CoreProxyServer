3fc5a1a24c396c2e20747eb8ebfaed7b69b3fbaa
SmithTom <943866961@qq.com>
2024-03-30 21:53:03 +0800

    1

---
+++ Impl/Class.cs
@@ -0,0 +1,13 @@
+﻿using Grpc.Core;
+using Grpc.Core.Interceptors;
+
+namespace ServerWebApplication.Impl
+{
+    public class Class : Interceptor
+    {
+        public override AsyncDuplexStreamingCall<TRequest, TResponse> AsyncDuplexStreamingCall<TRequest, TResponse>(ClientInterceptorContext<TRequest, TResponse> context, AsyncDuplexStreamingCallContinuation<TRequest, TResponse> continuation)
+        {
+            return base.AsyncDuplexStreamingCall(context, continuation);
+        }
+    }
+}
