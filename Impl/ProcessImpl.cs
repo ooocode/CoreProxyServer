e08f1730fe1bcd5698ea66adb28ba0c80074b513
SmithTom <943866961@qq.com>
2024-09-14 09:51:55 +0800

    1

--- Impl/ProcessImpl.cs
+++ Impl/ProcessImpl.cs
@@ -82,7 +82,30 @@ namespace ServerWebApplication.Impl
 
 
             CurrentCount.Inc();
-            var taskClient = HandlerClient(requestStream, target, cancellationToken);
+
+            try
+            {
+                await Parallel.ForAsync(0, 2, cancellationToken, async (t, c) =>
+                {
+                    if (t == 0)
+                    {
+                        await HandlerClient(requestStream, target, c);
+                    }
+                    else if (t == 1)
+                    {
+                        await HandlerServer(responseStream, target, c);
+                        await Task.Delay(1500, c);
+                        context.GetHttpContext().Abort();
+                    }
+                });
+            }
+            finally
+            {
+                CurrentCount.Dec();
+            }
+
+
+            /*var taskClient = HandlerClient(requestStream, target, cancellationToken);
             var taskServer = HandlerServer(responseStream, target, cancellationToken);
 
             try
@@ -103,7 +126,7 @@ namespace ServerWebApplication.Impl
             finally
             {
                 CurrentCount.Dec();
-            }
+            }*/
         }
 
         private static async Task HandlerClient(IAsyncStreamReader<SendDataRequest> requestStream, SocketConnect target, CancellationToken cancellationToken)
