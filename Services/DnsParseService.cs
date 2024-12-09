e1b28842b12846171b671a79f9f7f2dfc2a47eb3
oyk <943866961@qq.com>
2024-12-09 17:29:12 +0800

    1

--- Services/DnsParseService.cs
+++ Services/DnsParseService.cs
@@ -10,37 +10,33 @@ namespace ServerWebApplication.Services
             NameServer.Cloudflare,
             NameServer.Cloudflare2);
 
-        private static async Task<IPAddress?> ParseByDnsClientAsync(string hostName, CancellationToken cancellationToken)
+        private static async Task<IPAddress?> ParseByDnsClientAsync(string hostName, QueryType queryType, CancellationToken cancellationToken)
         {
             var result = await LookupClient.QueryAsync(hostName,
-               queryType: QueryType.ANY,
+               queryType: queryType,
                cancellationToken: cancellationToken);
             var records = result.Answers.ARecords().ToList();
 
-            var ipv4 = records.FirstOrDefault(x => x.RecordType == DnsClient.Protocol.ResourceRecordType.A);
-            if (ipv4 != null)
-            {
-                return ipv4.Address;
-            }
-
-            var ipv6 = records.FirstOrDefault(x => x.RecordType == DnsClient.Protocol.ResourceRecordType.AAAA);
-            if (ipv6 != null)
-            {
-                return ipv6.Address;
-            }
-
-            return null;
+            return records.FirstOrDefault()?.Address;
         }
 
         public async Task<IPAddress> GetIpAsync(string hostName, int _, CancellationToken cancellationToken = default)
         {
-            var iPAddress = await ParseByDnsClientAsync(hostName, cancellationToken);
-            if (iPAddress == null)
+            var iPAddress = await ParseByDnsClientAsync(hostName, QueryType.A, cancellationToken);
+            if (iPAddress != null)
             {
-                var ipAddresses = await Dns.GetHostAddressesAsync(hostName, cancellationToken);
-                iPAddress = ipAddresses.FirstOrDefault(x => x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
-                iPAddress ??= ipAddresses.FirstOrDefault(x => x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6);
+                return iPAddress;
             }
+
+            iPAddress = await ParseByDnsClientAsync(hostName, QueryType.AAAA, cancellationToken);
+            if (iPAddress != null)
+            {
+                return iPAddress;
+            }
+
+            var ipAddresses = await Dns.GetHostAddressesAsync(hostName, cancellationToken);
+            iPAddress = ipAddresses.FirstOrDefault(x => x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
+            iPAddress ??= ipAddresses.FirstOrDefault(x => x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6);
 
             ArgumentNullException.ThrowIfNull(iPAddress, nameof(iPAddress));
             return iPAddress;
