17c419cba6ebb5e13b226d867a6497697dc44ede
oyk <943866961@qq.com>
2024-12-09 15:07:47 +0800

    1

--- Services/DnsParseService.cs
+++ Services/DnsParseService.cs
@@ -8,6 +8,7 @@ namespace ServerWebApplication.Services
         {
             var ipAddresses = await Dns.GetHostAddressesAsync(hostName, cancellationToken);
             var iPAddress = ipAddresses.FirstOrDefault(x => x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
+            iPAddress ??= ipAddresses.FirstOrDefault(x => x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6);
             ArgumentNullException.ThrowIfNull(iPAddress, nameof(iPAddress));
 
             return iPAddress;
