cbe8adfc1e399e9ca727e55fd4afcce5b3fe11fb
oyk <943866961@qq.com>
2024-12-10 12:31:00 +0800

    1

--- Services/DnsParseService.cs
+++ Services/DnsParseService.cs
@@ -1,41 +1,40 @@
-using DnsClient;
 using System.Net;
 
 namespace ServerWebApplication.Services
 {
     public class DnsParseService
     {
-        public static readonly LookupClient LookupClient = new(NameServer.GooglePublicDns,
-            NameServer.GooglePublicDns2,
-            NameServer.Cloudflare,
-            NameServer.Cloudflare2);
+        //public static readonly LookupClient LookupClient = new(NameServer.GooglePublicDns,
+        //    NameServer.GooglePublicDns2,
+        //    NameServer.Cloudflare,
+        //    NameServer.Cloudflare2);
 
-        private static async Task<IPAddress?> ParseByDnsClientAsync(string hostName, QueryType queryType, CancellationToken cancellationToken)
-        {
-            var result = await LookupClient.QueryAsync(hostName,
-               queryType: queryType,
-               cancellationToken: cancellationToken);
-            var records = result.Answers.ARecords().ToList();
+        //private static async Task<IPAddress?> ParseByDnsClientAsync(string hostName, QueryType queryType, CancellationToken cancellationToken)
+        //{
+        //    var result = await LookupClient.QueryAsync(hostName,
+        //       queryType: queryType,
+        //       cancellationToken: cancellationToken);
+        //    var records = result.Answers.ARecords().ToList();
 
-            return records.FirstOrDefault()?.Address;
-        }
+        //    return records.FirstOrDefault()?.Address;
+        //}
 
         public async Task<IPAddress> GetIpAsync(string hostName, int _, CancellationToken cancellationToken = default)
         {
-            var iPAddress = await ParseByDnsClientAsync(hostName, QueryType.A, cancellationToken);
-            if (iPAddress != null)
-            {
-                return iPAddress;
-            }
+            //var iPAddress = await ParseByDnsClientAsync(hostName, QueryType.A, cancellationToken);
+            //if (iPAddress != null)
+            //{
+            //    return iPAddress;
+            //}
 
-            iPAddress = await ParseByDnsClientAsync(hostName, QueryType.AAAA, cancellationToken);
-            if (iPAddress != null)
-            {
-                return iPAddress;
-            }
+            //iPAddress = await ParseByDnsClientAsync(hostName, QueryType.AAAA, cancellationToken);
+            //if (iPAddress != null)
+            //{
+            //    return iPAddress;
+            //}
 
             var ipAddresses = await Dns.GetHostAddressesAsync(hostName, cancellationToken);
-            iPAddress = ipAddresses.FirstOrDefault(x => x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
+            var iPAddress = ipAddresses.FirstOrDefault(x => x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
             iPAddress ??= ipAddresses.FirstOrDefault(x => x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6);
 
             ArgumentNullException.ThrowIfNull(iPAddress, nameof(iPAddress));
