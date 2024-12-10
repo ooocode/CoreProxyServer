ec711d470be80757350326a34296d27c62f93c5c
oyk <943866961@qq.com>
2024-12-10 10:03:14 +0800

    1

--- Common/PipeReaderExtend.cs
+++ Common/PipeReaderExtend.cs
@@ -6,47 +6,10 @@ namespace ServerWebApplication.Common
 {
     public static class PipeReaderExtend
     {
-        /// <summary>
-        /// Reads all chunks of data from the pipe.
-        /// </summary>
-        /// <param name="reader">The pipe reader.</param>
-        /// <param name="token">The token that can be used to cancel the operation.</param>
-        /// <returns>A sequence of data chunks.</returns>
-        /// <exception cref="OperationCanceledException">The operation has been canceled.</exception>
-        //public static async IAsyncEnumerable<ReadOnlyMemory<byte>> ReadAllAsync(PipeReader reader,
-        //    [EnumeratorCancellation] CancellationToken token = default)
-        //{
-        //    ReadResult result;
-        //    do
-        //    {
-        //        result = await reader.ReadAsync(token).ConfigureAwait(false);
-
-        //        var buffer = result.Buffer;
-        //        var consumed = buffer.End;
-
-        //        try
-        //        {
-        //            if (buffer.IsSingleSegment)
-        //            {
-        //                yield return buffer.First;
-        //            }
-        //            else
-        //            {
-        //                yield return buffer.ToArray();
-        //            }
-        //        }
-        //        finally
-        //        {
-        //            reader.AdvanceTo(consumed);
-        //        }
-        //    } while (!result.IsCompleted);
-        //}
-
         public static async IAsyncEnumerable<ReadOnlyMemory<byte>> ReadAllAsync(
             PipeReader reader,
             [EnumeratorCancellation] CancellationToken token = default)
         {
-
             ReadResult result;
             do
             {
